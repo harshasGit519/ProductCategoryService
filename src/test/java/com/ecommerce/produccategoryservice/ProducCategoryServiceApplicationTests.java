863fc09ce5b20bacd6ab7c7f429349e812e3da20
harshasGit519 <shtpulluru@gmail.com>
2025-10-10 08:54:22 +0530

    BaseCommit

---
+++ src/test/java/com/ecommerce/produccategoryservice/ProducCategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.produccategoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducCategoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
